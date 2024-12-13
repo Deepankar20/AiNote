b454f48231e3a5787a7b0e6940f2addc142c9f3a
Deepankar20 <deepankardt2012@gmail.com>
2024-12-14 00:04:23 +0530

    tested some events and gemini api

--- server/index.ts
+++ server/index.ts
@@ -1,36 +1,19 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import express from "express"
+import http from "http";
+import SocketService from "./services/socketService";
 
-const genAI = new GoogleGenerativeAI(`${process.env.GEMINI_API_KEY}`);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+async function init() {
+  const socketService = new SocketService();
 
-const prompt = "generate an image of a person programming";
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT ? process.env.PORT : 8000;
 
-const result = await model.generateContent(prompt);
+  socketService.io.attach(httpServer);
 
-const app = express();
+  httpServer.listen(PORT, () => {
+    console.log(`HTTP Server started at PORT: ${PORT}`);
+  });
 
-// app.get('/stream', async (req, res) => {
-//   res.setHeader('Content-Type', 'text/event-stream');
-//   res.setHeader('Cache-Control', 'no-cache');
-//   res.setHeader('Connection', 'keep-alive');
+  socketService.initListeners();
+}
 
-//   const generateStream = async () => {
-//     const response = await model.generateStream({ prompt: req.query.prompt });
-//     for await (const chunk of response) {
-//       res.write(`data: ${chunk}\n\n`);
-//     }
-//     res.end();
-//   };
-
-//   generateStream().catch((err) => {
-//     console.error(err);
-//     res.write(`data: ${JSON.stringify({ error: 'Streaming failed' })}\n\n`);
-//     res.end();
-//   });
-// });
-
-app.listen(3000, () => console.log('Server is running on port 3000'));
-
-
-console.log(result.response.text());
+init();
--- server/services/socketService.ts
+++ server/services/socketService.ts
@@ -1,8 +1,11 @@
 import { Server, Socket } from "socket.io";
 import { EVENT } from "../types/types.ts";
+import { GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai";
 
 export default class SocketService {
   private _io: Server;
+  private genAI: GoogleGenerativeAI;
+  private model: GenerativeModel;
 
   constructor() {
     this._io = new Server({
@@ -11,17 +14,31 @@ export default class SocketService {
         origin: "*",
       },
     });
+
+    this.genAI = new GoogleGenerativeAI(`${process.env.GEMINI_API_KEY}`);
+    this.model = this.genAI.getGenerativeModel({
+      model: `${process.env.GEMINI_MODEL}`,
+    });
   }
 
   public initListeners() {
     const io = this._io;
 
     io.on("connect", (socket) => {
-      socket.on(EVENT.INPUT, async (data: {}) => {});
-
       socket.on(
-        EVENT.STREAM,
-        async (data: { id: string; command: string }) => {}
+        EVENT.INPUT,
+        async (data: { input: string; userId: string }) => {
+          try {
+            console.log(data);
+
+            const stream = await this.model.generateContent(data.input);
+            console.log(stream.response.text());
+
+            io.emit(EVENT.STREAM, stream);
+          } catch (error) {
+            console.error("Error generating text:", error);
+          }
+        }
       );
 
       socket.on(EVENT.UPDATE, async (data: { id: string }) => {});
